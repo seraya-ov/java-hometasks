640098d15ffec875b5f6760ecff4d68e4c5fa188
Olga Seraya <seraya.ov@phystech.edu>
2020-03-29 17:11:16 +0300

    fix hometask 3

--- Hometask3/src/main/java/ScalableThreadPool.java
+++ Hometask3/src/main/java/ScalableThreadPool.java
@@ -23,39 +23,42 @@ public class ScalableThreadPool implements ThreadPool {
             currentThreadCnt++;
             thread.start();
         }
+        for (int i = minThreadCnt; i < maxThreadCnt; ++i) {
+            Thread thread = new Thread(this::run);
+            threads.add(thread);
+        }
     }
 
     public void execute(Runnable runnable) {
         queue.add(runnable);
-        if (!queue.isEmpty()) {
-            int cur = currentThreadCnt;
-            for (int i = cur; i < maxThreadCnt; ++i) {
-                Thread thread = new Thread(this::run);
-                threads.add(thread);
-                currentThreadCnt++;
-                thread.start();
-            }
-        }
-        else {
-            for (int i = currentThreadCnt; i >= minThreadCnt; --i) {
-                threads.get(i).stop();
-                currentThreadCnt--;
-            }
-        }
     }
 
     private void run() {
         try {
             while (true) {
                 synchronized (this) {
-                    if (!queue.isEmpty()) {
+                    if (queue.isEmpty()) {
+                        for (int i = currentThreadCnt; i > minThreadCnt; --i) {
+                            threads.get(i - 1).interrupt();
+                            currentThreadCnt--;
+                        }
+                    }
+                    else {
                         Runnable runnable = queue.remove();
                         runnable.run();
+                        if (!queue.isEmpty()) {
+                            int cur = currentThreadCnt;
+                            for (int i = cur; i < maxThreadCnt; ++i) {
+                                Thread thread = threads.get(i);
+                                currentThreadCnt++;
+                                thread.start();
+                            }
+                        }
                     }
                 }
             }
         } catch (Exception exc) {
-            System.out.println("error");
+            System.out.println(exc);
         }
     }
 }
