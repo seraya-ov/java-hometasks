26229726913142407a70792b0ed1896596c74698
Olga Seraya <seraya.ov@phystech.edu>
2020-04-16 21:26:42 +0300

    add hometask 3

---
+++ Hometask3.2/src/main/java/Task.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.Callable;
+
+public class Task<T> {
+    private final Callable<? extends T> callable;
+    private volatile boolean finished;
+    private volatile T result;
+    private RuntimeException e;
+
+    public Task(Callable<? extends T> callable) {
+        this.callable = callable;
+        this.finished = false;
+    }
+
+    public T get() {
+        try {
+            if (finished) return result;
+            if (e != null) throw e;
+            synchronized (this) {
+                if (finished) return result;
+                if (e != null) throw e;
+                result = callable.call();
+                finished = true;
+                return result;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            throw this.e = new RuntimeException(e);
+        }
+    }
+}
